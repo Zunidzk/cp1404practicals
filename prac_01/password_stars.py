dc90dba9f07a8181234a04d15e2fd7dd8bcf74f3
Zunid <zunidzk@gmail.com>
2023-03-24 14:10:24 +0800

    Add prac 1 files

--- prac_01/password_stars.py
+++ prac_01/password_stars.py
@@ -1,8 +1,15 @@
-nums1 = [1, -5, 2, 0, 4, 2, -3]
-nums2 = [1, -5, 2, 4, 4, 2, 7]
-result = 0
-j = 0
-while j < len(nums1):
-    if nums1[j] != nums2[j]:
-        result = result + 1
-    j = j + 1
+def main():
+    length = get_password_length()
+    while length < 12:
+        print("Password too short!")
+        length = get_password_length()
+    print('*' * length)
+
+
+def get_password_length():
+    password = input("Enter Your Password: ")
+    length = len(password)
+    return length
+
+
+main()
--- prac_01/temperatures.py
+++ prac_01/temperatures.py
@@ -1,19 +1,32 @@
-MENU = """C - Convert Celsius to Fahrenheit
-F - Convert Fahrenheit to Celsius
-Q - Quit"""
-print(MENU)
-choice = input(">>> ").upper()
-while choice != "Q":
-    if choice == "C":
-        celsius = float(input("Celsius: "))
-        fahrenheit = celsius * 9.0 / 5 + 32
-        print(f"Result: {fahrenheit:.2f} F")
-    elif choice == "F":
-        fahrenheit = float(input("Fahrenheit: "))
-        celsius = 5 / 9 * (fahrenheit - 32)
-        print(f"Result: {celsius: .2f} C")
-    else:
-        print("Invalid option")
+MENU = ("C - Convert Celsius to Fahrenheit\nF - Convert Fahrenheit to Celsius\nQ - Quit")
+
+
+def main():
     print(MENU)
     choice = input(">>> ").upper()
-print("Thank you.")
+    while choice != "Q":
+        if choice == "C":
+            fahrenheit = con_celsius()
+            print(fahrenheit)
+        elif choice == "F":
+            celsius = con_fahrenheit()
+            print(celsius)
+        else:
+            print("Invalid option")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Thank you.")
+
+
+def con_celsius():
+    celsius = float(input("Celsius: "))
+    fahrenheit = celsius * 9.0 / 5 + 32
+    return fahrenheit
+
+
+def con_fahrenheit():
+    fahrenheit = float(input("Fahrenheit: "))
+    celsius = 5 / 9 * (fahrenheit - 32)
+    return celsius
+
+main()
