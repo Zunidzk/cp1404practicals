9b8f82fbb43cf248a23bd0d81e9692ea46ae6cde
Zunid <zunidzk@gmail.com>
2023-04-09 17:57:02 +0800

    Add prac 1 files

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -1,9 +1,12 @@
-quick_picks = input(int("Number of quick picks : "))
-lines = []
 import random
-number = random.randint(1, 45)
-for i in range(0, 6):
-    lines = lines.append(number)
 
 
-
+quick_picks = int(input("Number of quick picks : "))
+for i in range(quick_picks):
+    lines = []
+    while len(lines) < 6:
+        numbers = random.randint(1, 45)
+        if numbers not in lines:
+            lines.append(numbers)
+    lines.sort()
+    print("{:3} {:3} {:3} {:3} {:3} {:3}".format(*lines))
--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -8,6 +8,12 @@ FILENAME = "subject_data.txt"
 def main():
     data = get_data()
     print(data)
+    print("----------")
+    for part in data:
+        code = part[0]
+        teacher = part[1]
+        students = part[2]
+        print(f"{code} is taught by {teacher:12} and has {students:>3} students")
 
 
 def get_data():
@@ -19,17 +25,19 @@ def get_data():
             lines.append(line[:-1])
         else:
             lines.append(line)
-        print(f"{lines}")
-        print("----------")
         """print(line)  # See what a line looks like
         print(repr(line))  # See what a line really looks like
         line = line.strip('\n')  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
         print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked"""
-        print("----------")
+        print(parts)  # See if that worked
+        print("----------")"""
     input_file.close()
     parts = [line.split(',') for line in lines]
-    print(parts)
+    for part in parts:
+        part[2] = int(part[2])
+    return parts
+
+
 main()
