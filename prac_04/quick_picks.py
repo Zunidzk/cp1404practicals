ed080ced95c70480cfbb3066322148724a0ae683
Zunid <zunidzk@gmail.com>
2023-04-09 14:28:42 +0800

    Add prac 1 files

---
+++ prac_04/quick_picks.py
@@ -0,0 +1,9 @@
+quick_picks = input(int("Number of quick picks : "))
+lines = []
+import random
+number = random.randint(1, 45)
+for i in range(0, 6):
+    lines = lines.append(number)
+
+
+
--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -2,7 +2,6 @@
 CP1404/CP5632 Practical
 Data file -> lists program
 """
-
 FILENAME = "subject_data.txt"
 
 
@@ -13,17 +12,24 @@ def main():
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
-    input_file = open(FILENAME)
+    lines = []
+    input_file = open(FILENAME, 'r')
     for line in input_file:
-        print(line)  # See what a line looks like
+        if line[-1] == '\n':
+            lines.append(line[:-1])
+        else:
+            lines.append(line)
+        print(f"{lines}")
+        print("----------")
+        """print(line)  # See what a line looks like
         print(repr(line))  # See what a line really looks like
-        line = line.strip()  # Remove the \n
+        line = line.strip('\n')  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
         print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
+        print(parts)  # See if that worked"""
         print("----------")
     input_file.close()
-
-
+    parts = [line.split(',') for line in lines]
+    print(parts)
 main()
